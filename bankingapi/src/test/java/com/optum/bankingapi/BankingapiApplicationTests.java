46d8a6329accc7ea844877cc8bb4a39d0cd9d12b
Parameswari Bala <parameswaribala@gmail.com>
2024-01-17 14:39:25 +0530

    updated

---
+++ bankingapi/src/test/java/com/optum/bankingapi/BankingapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optum.bankingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
