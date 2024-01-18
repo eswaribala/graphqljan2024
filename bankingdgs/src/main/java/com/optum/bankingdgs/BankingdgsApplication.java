08deb4015ab69fed31fda82fe290b2d7aab8bdf1
Parameswari Bala <parameswaribala@gmail.com>
2024-01-18 17:00:33 +0530

    updated

---
+++ bankingdgs/src/main/java/com/optum/bankingdgs/BankingdgsApplication.java
@@ -0,0 +1,13 @@
+package com.optum.bankingdgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingdgsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingdgsApplication.class, args);
+    }
+
+}
---
+++ bankingdgs/src/test/java/com/optum/bankingdgs/BankingdgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optum.bankingdgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingdgsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
