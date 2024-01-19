02a52155352b1eea08dd693948b345f3da85d83c
Parameswari Bala <parameswaribala@gmail.com>
2024-01-19 11:39:14 +0530

    updated

---
+++ bankingjavaclient/src/main/java/com/optum/bankingjavaclient/BankingjavaclientApplication.java
@@ -0,0 +1,13 @@
+package com.optum.bankingjavaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingjavaclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingjavaclientApplication.class, args);
+    }
+
+}
---
+++ bankingjavaclient/src/test/java/com/optum/bankingjavaclient/BankingjavaclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optum.bankingjavaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingjavaclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
